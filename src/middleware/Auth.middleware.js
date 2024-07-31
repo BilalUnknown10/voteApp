912e5e6eceb89eed88f7fda1dc6506f8e0022c18
BilalUnknown10 <bilal9342610@gmail.com>
2024-07-31 17:29:37 +0500

    fix some mistakes

--- src/middleware/Auth.middleware.js
+++ src/middleware/Auth.middleware.js
@@ -5,13 +5,13 @@ const authMiddleware = async (req, res, next) => {
     const token = req.cookies.accessToken || req.header("Authorization").replace("Bearer ");
 
     if(!token){
-        return res.status(200).json("Unauthorized request")
+        return res.status(400).json("Unauthorized request")
     }
 
     const verifyToken = jwt.verify(token, process.env.TOKEN_SECRET_KEY)
 
     if(!verifyToken){
-        return res.status(200).json("Invalid Token")
+        return res.status(400).json("Invalid Token")
     }
 
     const voter = await Voter.findById(verifyToken._id)
