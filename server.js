4d31051a94234f3cc00a099837f4f887d54a80fb
BilalUnknown10 <bilal9342610@gmail.com>
2024-08-23 17:17:23 +0500

    add front end url

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended : true}));
 app.use(cookie());
 
 const corsOptions = {
-    origin: process.env.CORS_ORIGION,
+    origin: "https://voting-app-front-end.vercel.app",
     credentials : true
 }
 
