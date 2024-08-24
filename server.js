4bcd15c9a6c7775a160704ed0390af59edf3c61a
BilalUnknown10 <bilal9342610@gmail.com>
2024-08-24 15:27:55 +0500

    change front end url

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended : true}));
 app.use(cookie());
 
 const corsOptions = {
-    origin : ["http://localhost:5173"],
+    origin : ["https://votin-app-front-end.vercel.app"],
     methods : ["GET", "POST", "PATCH", "DELETE", "PUT"],
     credentials : true
 }
