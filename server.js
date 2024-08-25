b3f5ef2c69c9fda5138cd725324c5869272586f6
BilalUnknown10 <bilal9342610@gmail.com>
2024-08-25 10:10:36 +0500

    changes in context file

--- server.js
+++ server.js
@@ -16,13 +16,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 
 
-const corsOptions = {
-    // origin : ["https://votin-app-front-end.vercel.app"],
-    // methods : ["GET", "POST", "PATCH", "DELETE", "PUT"],
-    credentials : true
-}
+// const corsOptions = {
+//     // origin : ["https://votin-app-front-end.vercel.app"],
+//     // methods : ["GET", "POST", "PATCH", "DELETE", "PUT"],
+//     credentials : true
+// }
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(cookie());
 // app.use(bodyParser);
 
