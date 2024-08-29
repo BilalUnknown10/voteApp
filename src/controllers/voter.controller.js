00ca7c2e80d5b6b5e8d8e8cefe95d5342a3dbe12
BilalUnknown10 <bilal9342610@gmail.com>
2024-08-29 15:32:29 +0500

    changes response messages

--- src/controllers/voter.controller.js
+++ src/controllers/voter.controller.js
@@ -218,5 +218,4 @@ module.exports = {
     votePole,
     checkUserLogin,
     voteCount,
-    home
 }
--- src/routes/Voter.routes.js
+++ src/routes/Voter.routes.js
@@ -16,7 +16,6 @@ const router = Router();
 router.route("/registration").post(registration);
 router.route('/login').post(login);
 router.route('/count/:party').get(voteCount)
-router.route('/home').get(home)
 // secured routes....
 router.route("/logOut").get(auth, logOut);
 router.route("/polevote").post(auth, votePole),
