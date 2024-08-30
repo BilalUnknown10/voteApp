a548b537efd3250114ffab85ba857a201e1738d1
BilalUnknown10 <bilal9342610@gmail.com>
2024-08-30 15:04:56 +0500

    add local url for testing

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 4000;
 connection();
 
 const corsOptions = {
-    origin : ["https://votin-app-front-end.vercel.app"],
+    origin : ["https://votin-app-front-end.vercel.app","http://localhost:5173"],
     methods : ["GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS"],
     credentials : true
 }
